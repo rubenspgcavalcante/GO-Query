61c14781930a424f3dc8d4836f41817cac302611
trix-rubens <trix-rubens@trixlog.com>
2013-10-17 15:15:27 -0300

    All tests from Query object

--- tests/nodeunit/Query.test.js
+++ tests/nodeunit/Query.test.js
@@ -13,10 +13,33 @@ module.exports = testCase({
 
     Methods: function(test){
         var query = new GO.Query(data);
-        test.expect(3);
         test.ok(query.hasOwnProperty('select'));
         test.ok(query.hasOwnProperty('update'));
         test.ok(query.hasOwnProperty('remove'));
         test.done();
+    },
+
+    GetFrom: function(test){
+        var query = new GO.Query(data);
+        test.ok(query.select() instanceof GO.Core.From);
+        test.ok(query.update() instanceof GO.Core.From);
+        test.ok(query.remove() instanceof GO.Core.From);
+        test.done();
+    },
+
+    ValidateRecord: function(test){
+        var query = new GO.Query(data);
+
+        query.select();
+        test.ok(query._getRecord().type == GO.query.type.SELECT);
+
+        query.update();
+        test.ok(query._getRecord().type == GO.query.type.UPDATE);
+
+        query.remove();
+        test.ok(query._getRecord().type == GO.query.type.DELETE);
+
+        test.done();
     }
+
 });
